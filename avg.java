4fec0465bb4e96a584aa5bd37e276c8ef05ef39b
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-25 19:51:29 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,16 @@
+class app
+  {
+    public static void main(String,args[])
+    {
+int[] q =  {10,20,30,40,50};
+      int sum =0;
+      for(int i=0;i<q.length;i++)
+        {
+
+          sum += a[i];
+          
+        }
+      System.out.println(sum/q.length);
+      
+    }
+  }
